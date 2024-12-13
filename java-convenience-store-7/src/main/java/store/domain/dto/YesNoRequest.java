79bec940c318d94d2fda37951a35b0e45d2b0f99
이희수 <hslee9234@naver.com>
2024-12-14 01:53:05 +0900

    feat: Y/N dto 구현

---
+++ java-convenience-store-7/src/main/java/store/domain/dto/YesNoRequest.java
@@ -0,0 +1,24 @@
+package store.domain.dto;
+
+import store.exception.StoreException;
+
+public class YesNoRequest {
+    private final boolean response;
+    public YesNoRequest(String response){
+        this.response = toBoolean(response);
+    }
+
+    public boolean getResponse(){
+        return response;
+    }
+
+    private boolean toBoolean(String response){
+        if(response.equals("Y")){
+            return true;
+        }
+        if(response.equals("N")){
+            return false;
+        }
+        throw new StoreException("응답은 Y/N 으로 해주세요.");
+    }
+}
