6609350a33a2b2cb055b9b8bb3ba512df49bc939
이희수 <hslee9234@naver.com>
2024-12-14 01:52:12 +0900

    refactor: OutputView

--- java-convenience-store-7/src/main/java/store/view/OutputView.java
+++ java-convenience-store-7/src/main/java/store/view/OutputView.java
@@ -3,9 +3,12 @@ package store.view;
 import store.domain.Products;
 
 public class OutputView {
-    public static void printProducts(Products products){
+    public static void printProducts(String products){
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\r\n");
-        System.out.println(products.toString());
+        System.out.println(products);
+    }
+    public static void print(String output){
+        System.out.println(output);
     }
 }
